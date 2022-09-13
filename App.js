22b31314ab98d16f8ce08aa5f7cd50d6fbc1903d
EMMANUEL <dammylola299@gmail.com>
2022-09-13 13:58:56 +0100

    Fixed module not working bug---issue was with the paths in the imports

--- App.js
+++ App.js
@@ -1,16 +1,20 @@
-import Nav from "./modules/nav";
-import Bio from "./modules/bio";
-import Gallery from "./modules/gallery";
+import Nav from "./modules/nav.js";
+import Bio from "./modules/bio.js";
+import Gallery from "./modules/gallery.js";
 
 
 const App = () => {
     return `
         ${Nav()}
+        <div class="container">
         ${Bio()}
         ${Gallery()}
+        </div>
     `
 }
 
+
+// using query selector to query the dom
 // const root = document.querySelector('#root');
 // root.innerHTML = App();
 

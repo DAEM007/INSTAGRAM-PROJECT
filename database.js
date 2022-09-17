4ba9cf1df21e9baa2990d7aa65b0cd1ac4faaf99
EMMANUEL <dammylola299@gmail.com>
2022-09-17 18:01:50 +0100

    Added entry to the database

--- database.js
+++ database.js
@@ -1,19 +1,21 @@
 
+// SETTING UP THE DATABASE
 // Request to open the database
-const request = indexedDB.open('Instagram', 2);
+const request = indexedDB.open('Instagram', 5);
 
 // checking for success
 request.onsuccess = () => {
     const database =  request.result;
     console.log('Suceess in creating or acessing database!!!');
     console.log(database);
+
 }
 
+// Run when there is an upgrade in the version
 request.onupgradeneeded = () => {
-    console.log('Hey Bio!!!');
     const database = request.result;
-    database.createObjectStore('bio');
-    database.createObjectStore('gallery');
+    database.createObjectStore('bio', {autoIncrement: true});
+    database.createObjectStore('gallery', {autoIncrement: true});
 }
 
 // checking for error
@@ -21,4 +23,13 @@ request.onerror = () => {
     console.log('Error in creating or accessing database!!!');
 }
 
-export { request };
+const addEntryToDb = (storeName, entry) => {
+    const database = request.result;
+    // Request to add to the database
+    const transaction = database.transaction([storeName], 'readwrite');
+    const store = transaction.objectStore(storeName);
+    store.add(entry);
+
+}
+
+export { request, addEntryToDb };

e011e5e025fef5bfd2068b20a8f22aa8a181ce8c
EMMANUEL <dammylola299@gmail.com>
2022-09-20 13:20:32 +0100

    Working on the form animation

--- modules/Bio/editBio.js
+++ modules/Bio/editBio.js
@@ -2,11 +2,15 @@
 const editBioForm = () => {
     return `
     <form class="edit-bio-form">
-        <input type="text" id="bioName" placeholder="Name">
-        <input type="text" id="bioDescription" placeholder="Description">
-        <button type="submit">submit</button>
+        <input type="text" id="bioName" placeholder="Name" required>
+        <input type="text" id="bioDescription" placeholder="Description" required>
+        </br>
+        <button id="submit" type="submit">submit</button>
+        <button id="cancel" type="submit">Cancel</button>
     </form>
     `
 }
+
+
 
 export default editBioForm;
--- modules/Bio/events.js
+++ modules/Bio/events.js
@@ -5,11 +5,22 @@ const addBioEventListeners = () => {
     // Reference to the dom for the edit bio form
     const editBioForm = document.querySelector('.edit-bio-form');
     const editBioButton = document.querySelector('#editBioButton');
+    const cancelBioButton = document.querySelector('#cancel');
 
     // Event listener for the edit bio button before the edit bio form...recall order matters in your code
     editBioButton.addEventListener('click', (e) => {
         e.preventDefault();
-        editBioForm.style.display = 'block';
+        // editBioForm.style.display = 'block';
+        if(editBioForm.style.display = 'none'){
+            editBioForm.style.display = 'block';
+            editBioForm.animate({
+                opacity: [0 , 1],
+            },
+               {
+                fill: "forwards",  duration: 2000
+               }        
+            )
+        }
     })
 
     // Event listener to form for changes in the Bio name
@@ -26,6 +37,22 @@ const addBioEventListeners = () => {
 
         // add entry function for the database
         addEntryToDb('bio', {bioName, bioDescription});
+    })
+
+    // Event listener to the cancel button to change the display of the edit bio form
+    cancelBioButton.addEventListener('click', (e) => {
+        e.preventDefault();
+        // editBioForm.style.display = 'none';
+        if(editBioForm.style.display = 'block'){
+            editBioForm.style.display = 'none';
+            editBioForm.animate({
+                opacity: [0, 1],
+            },
+                {
+                    fill: "backwards", duration: 2000
+                }
+            )
+        }
     })
 
 }

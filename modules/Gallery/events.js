4c8aa2138a63785e3ff4f53febaec2f1d5cf18b9
EMMANUEL <dammylola299@gmail.com>
2022-09-23 11:31:39 +0100

    Trying to clear entries from the gallery---still in comments...need a good reason for that

--- modules/Gallery/events.js
+++ modules/Gallery/events.js
@@ -1,25 +1,24 @@
-import { addEntryToDb } from "../../database.js";
+import { addEntryToDb, clearAllEntries } from "../../database.js";
 
 const addGalleryEventListeners = () => {
     // Get a reference to the photo input
     const photoInput = document.querySelector('#add-photo-input');
-    const gallery = document.querySelector('.gallery');
     // Add an event listener to the photo input to display the photos...note it is a change event
     photoInput.addEventListener('change', () => {
         console.log(photoInput.files[0]);
+        // We would have to use a file reader to read the url of the file from the local disk...hence we use File reader object in js
         const reader = new FileReader();
         reader.readAsDataURL(photoInput.files[0]);
 
+        // clear entries from gallery thread before adding another entry to the database
+        // clearAllEntries('gallery');
+
         // Event for the file url
         reader.addEventListener('load', () => {
+            // Adding entry to the gallery store in the database
             addEntryToDb('gallery', reader.result);
         })
-        // gallery.innerHTML = `
-        //     ${Gallery()}
-        //     <a href="#" class="item">
-        //         <img src="${photoInput.files[0]}" alt="">
-        //     </a>
-        // `
+
     })
 }
 
